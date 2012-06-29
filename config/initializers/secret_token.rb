52b48bb894cfa845241e5ea5671318947ca06c7b
cnaize <cnaize@mail.ru>
2012-06-29 14:39:43 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyVeryFirstRailsApp::Application.config.secret_token = 'ef748fa85447aa5685ec03ee9cccd3662d1171f4da3e5afb6302673a4b1c05ae7bd09974b24a58a2911cd9f6b1615fcf5dc537d4a7a01682d38f823666f71e50'
